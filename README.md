7a098a845f51bee641d31145febe2fc66914f6a5
liaoli <277218962@qq.com>
2023-02-27 10:04:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# template_app
+
+A new Empty Flutter project for fast development.
+
+## Getting Started
+
---
+++ android/app/src/main/java/com/liaoli/temp/template_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.liaoli.temp.template_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
